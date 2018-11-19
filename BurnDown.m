26de9cbbb71d9b03fc544f8073d68fc8810ced7e
Erik Forsberg <erikforsberg95@hotmail.com>
2018-11-19 14:11:42 +0100

    fixing colors attempt 1

--- BurnDown.m
+++ BurnDown.m
@@ -9,26 +9,26 @@ if (rand<f)
         tempburningTrees = [];
         
         for i = 1:size(burningTrees,1)
-            if(area(burningTrees(i,1),burningTrees(i,2)) == 1)
+            if(area(burningTrees(i,1),burningTrees(i,2)) == 2)
                 tree = burningTrees(i,:);
-                area(tree(1), tree(2)) = 2;
+                area(tree(1), tree(2)) = 1;
                 
                 try
-                    if(area(tree(1,1)+1, tree(1,2)) == 1)
+                    if(area(tree(1,1)+1, tree(1,2)) == 2)
                         tempburningTrees = [tempburningTrees; [tree(1,1)+1, tree(1,2)]];
                     end
                 catch
                 end
                 
                 try
-                    if(area(tree(1,1), tree(1,2)+1) == 1)
+                    if(area(tree(1,1), tree(1,2)+1) == 2)
                         tempburningTrees = [tempburningTrees; [tree(1,1), tree(1,2)+1]];
                     end
                 catch
                 end
                 
                 try
-                    if(area(tree(1,1)-1, tree(1,2)) == 1)
+                    if(area(tree(1,1)-1, tree(1,2)) == 2)
                         tempburningTrees = [tempburningTrees; [tree(1,1)-1, tree(1,2)]];
                     end
                 catch
--- ForestFire1.m
+++ ForestFire1.m
@@ -1,15 +1,14 @@
 %forestFire 1
 
-timeSteps = 1000;
+timeSteps = 10000;
 areaSize = 128;
 
-f = 0.01;
-p = 0.005;
+f = 0.05;
+p = 0.01;
 
 area = zeros(areaSize, areaSize);
-mapTree = [1 1 1; 0 1 0];
-mapBurn = [1 1 1; 0 1 0; 1 0 0];
-colormap(mapTree);
+mapBurn = [1 1 1; 1 0 0; 0 1 0];
+colormap(mapBurn);
 plot1 = imagesc('CData',area);
 xlim([0 areaSize]);
 ylim([0 areaSize]);
@@ -19,20 +18,18 @@ t = 0;
 while(t<timeSteps)
     t = t+1;
     
-    area = TreeGrowth(area, p);    
+    area = TreeGrowth(area, p);
     set(plot1, 'CData', area);
+    drawnow
     
     area = BurnDown(area, f);
     
-    if(sum(area(:) == 2) ~= 0)
-        colormap(mapBurn);
+    if(sum(area(:) == 1) ~= 0)
         set(plot1, 'CData', area);
-        pause
-        colormap(mapTree);
+        drawnow
     end
     
-    
-    
+    area(area==1) = 0;  
     
     
 end
