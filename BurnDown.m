80c4776303fb95fec68fcfe25f5ffe9942eb9163
Erik Forsberg <erikforsberg95@hotmail.com>
2018-11-19 12:42:56 +0100

    burndown

--- BurnDown.m
+++ BurnDown.m
@@ -2,17 +2,48 @@ function area = BurnDown(area, f)
 
 if (rand<f)
     
-    i = randi(size(arean,1));
-    j = randi(size(area,2));
-    
-    burningTrees = [i, j];
+    burningTrees = [randi(size(area,1)), randi(size(area,2))];
     
     while(numel(burningTrees)>0)
-    
-    if(area(burningTrees) == 1))
         
+        tempburningTrees = [];
         
-    end
+        for i = 1:size(burningTrees,1)
+            if(area(burningTrees(i,1),burningTrees(i,2)) == 1)
+                tree = burningTrees(i,:);
+                area(tree(1), tree(2)) = 2;
+                
+                try
+                    if(area(tree(1,1)+1, tree(1,2)) == 1)
+                        tempburningTrees = [tempburningTrees; [tree(1,1)+1, tree(1,2)]];
+                    end
+                catch
+                end
+                
+                try
+                    if(area(tree(1,1), tree(1,2)+1) == 1)
+                        tempburningTrees = [tempburningTrees; [tree(1,1), tree(1,2)+1]];
+                    end
+                catch
+                end
+                
+                try
+                    if(area(tree(1,1)-1, tree(1,2)) == 1)
+                        tempburningTrees = [tempburningTrees; [tree(1,1)-1, tree(1,2)]];
+                    end
+                catch
+                end
+                
+                try
+                    if(area(tree(1,1), tree(1,2)-1) == 1)
+                        tempburningTrees = [tempburningTrees; [tree(1,1), tree(1,2)-1]];
+                    end
+                catch
+                end
+                
+            end
+        end
+        burningTrees = tempburningTrees;
     end
 end
-        
+end
