87684886e03f8a5c629c36118e05b771f6fec3d5
Erik Forsberg <erikforsberg95@hotmail.com>
2018-11-19 16:12:22 +0100

    finished 1

--- ForestFire1.m
+++ ForestFire1.m
@@ -1,33 +1,55 @@
 %forestFire 1
 
 timeSteps = 10000;
+nrFires = 1000;
 areaSize = 128;
+fireSizeVec = [];
+area = zeros(areaSize, areaSize);
+plotForest = false;
 
 f = 0.03;
 p = 0.001;
 
-area = zeros(areaSize, areaSize);
-map = [1 1 1; 1 0 0; 0 1 0];
-colormap(map);
-plot1 = imagesc('CData',area);
-xlim([0 areaSize]);
-ylim([0 areaSize]);
-
+if plotForest
+    subplot(1,2,1);
+    map = [1 1 1; 1 0 0; 0 1 0];
+    colormap(map);
+    plot1 = imagesc('CData',area);
+    xlim([0 areaSize]);
+    ylim([0 areaSize]);
+end
 
 t = 0;
-while(t<timeSteps)
+while(size(fireSizeVec, 1) < nrFires)
     t = t+1;
     
     area = TreeGrowth(area, p);
-    set(plot1, 'CData', area);
-    drawnow
     
-    area = BurnDown(area, f);
-    if(sum(area(:) == 1) ~= 0)
+    if plotForest
         set(plot1, 'CData', area);
         drawnow
     end
+    
+    area = BurnDown(area, f);
+    
+    if(sum(area(:) == 1) ~= 0)
+        fireSizeVec = [fireSizeVec; sum(area(:) == 1)];
+        
+        if plotForest
+            set(plot1, 'CData', area);
+            drawnow
+        end
+    end
     area(area == 1) = 0;
     
-    
 end
+
+fireSizeVec = fireSizeVec./areaSize^2;
+fireSizeVec = fliplr(sort(fireSizeVec));
+
+xValues = fliplr((1:nrFires)/nrFires);
+
+subplot(1,2,2);
+loglog(xValues,fireSizeVec,'.k', 'MarkerSize',2);
+axis equal
+pbaspect([1 1 1]); 
